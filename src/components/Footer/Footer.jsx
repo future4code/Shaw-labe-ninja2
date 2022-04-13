cf26eb7106ff085435c0f13c9030e5cf2a520611
Bruno Siqueira <santoserobinho@gmail.com>
2022-04-13 10:43:25 -0300

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'; 
 import { FooterContainer } from './style';
+import { LogoContainer } from './style';
+import { InformationsContainer } from './style';
+import logo from '../../img/logo.png'
 
 
 
@@ -12,7 +15,13 @@ export default class Footer extends React.Component {
 		return (
 			
 			<FooterContainer>
-	
+				<LogoContainer>
+					<img src={logo} />
+				</LogoContainer>
+				<InformationsContainer>
+					<p>Copyright © 2022-2022 Ebazar.com.br LTDA. <br/>
+CNPJ n.º 00.000.000/0000-00 / Av. das Nações Unidas, nº 3.003, Bonfim, Osasco/SP - CEP 06233-903 - Labeninjas S.A.</p>
+				</InformationsContainer>
 			</FooterContainer>
 		)
 
