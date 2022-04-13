9695eb8f747111a8c7c431b8e9c326dc8d935d2c
RaniSales <Ranievly5@hotmail.com>
2022-04-13 14:29:37 -0300

    change-page

--- src/pages/Screens/Screens.jsx
+++ src/pages/Screens/Screens.jsx
@@ -3,22 +3,49 @@ import HomeScreen from '../HomeScreen/HomeScreen';
 import Cart from '../Cart/Cart';
 import Footer from '../../components/Footer/Footer';
 import {ScreensContainer} from './style'
+import { List } from '@mui/material';
+import Header from '../../components/Header/Header';
 
 //LOGICA DE TROCA DE TELA AQUI
 
 //TELAS HOMESCREEN (PRINCIPAL)
 //      CART (CARRINHO)
 
-
 export default class Screens extends React.Component {
+
+	state= {
+		page:"Cart"
+	}
+
+	choosePage = () => {
+		switch(this.state.page){
+
+			case "Cart":
+			return <Cart />
+
+			case "HomeScreen":
+			return  <HomeScreen />
+		
+			default:
+			return <HomeScreen />
+		}
+
+	}
+
+	changePage = (screen) => {
+		this.setState({page:screen})
+	}
+	
 
 	render() {
 
 		return (
 			<ScreensContainer>
-                <HomeScreen/>{/* OU  <Cart/>  */}
+				<Header/> 
+				<button onClick={() => this.changePage("Cart")}>Carrinho</button>
+				<button onClick={() => this.changePage("HomeScreen")}>HomeScreen</button>
+				{this.choosePage()}
 
-               
 				<Footer/>
 			</ScreensContainer>
 		)
