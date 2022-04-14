a5b0686bd59bd173d1074a956d33fff1e98a96db
Bruno Siqueira <santoserobinho@gmail.com>
2022-04-14 09:05:24 -0300

    promotions segunda versão

--- src/components/Promotions/Promotions.jsx
+++ src/components/Promotions/Promotions.jsx
@@ -1,8 +1,10 @@
 import React from 'react'; 
 import { MainContainer } from './style';
 import { PromotionsBox } from './style';
-
-
+import JobCard from '../JobCard/JobCard';
+import { FieldSet } from './style';
+import { Legend } from './style';
+import OFF from '../../img/OFF.png'
 
 export default class Promotions extends React.Component {
 
@@ -10,10 +12,15 @@ export default class Promotions extends React.Component {
 
 		return (
 			<MainContainer>
-				<PromotionsBox>
-					//Cards
-				</PromotionsBox>
-			</MainContainer>
+				<FieldSet class="fieldset-border">
+					<Legend >Promoções do dia <img src={OFF}/></Legend>
+					<PromotionsBox>
+						{/* <JobCard/>
+						<JobCard/>
+						<JobCard/> */}
+					</PromotionsBox>
+				</FieldSet>
+			</MainContainer>	
 		)
 
 	}
