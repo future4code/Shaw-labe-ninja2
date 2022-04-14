26d11bea3a69e79245f15e223651a680ef7dfd97
RaniSales <Ranievly5@hotmail.com>
2022-04-14 10:28:27 -0300

    att

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -26,66 +26,52 @@ export default class Filter extends React.Component {
 		sortingParameter:"title",
 		order:"asc",
 	}
-
 	updateQuery = (event) => {
 		this.setState({
 			query: event.target.value
 		})
 	}
-
 	updateMinPrice = (event) => {
 		this.setState({
 			minPrice: event.target.value
 		})
 	}
-
 	updateMaxPrice = (event) => {
 		this.setState({
 			maxPrice: event.target.value
 		})
 	}
-
 	updateSortingParameter = (event) => {
 		this.setState({
 			sortingParamete: event.target.value
 		})
 	}
-
 	updateOrder = (event) => {
 		this.setState({
 			order: event.target.value
 		})
 	}
 	render() {
-
 		return <>
-
 		export funcion Filter(props){
 			return
 				<input
 					placeholder="search"
 					value={props.query}
 					onChange={this.updateQuery}
-
 				/>
-
 				<input
 					type='number'
 					placeholder="Min Price"
 					value={props.minPrice}
 					onChange={this.updateMinPrice}
-
 				/>
-
 				<input
 					type='number'
 					placeholder="Max Price"
 					value={props.maxPrice}
 					onChange={this.updateMaxPrice}
-
 				/>
-
-
 			<span>
 			<label for="sort">Ordination</label>
 				<select 
@@ -97,7 +83,6 @@ export default class Filter extends React.Component {
 					<option value="Price">Price</option>
 					<option value="dueDate">Term</option>
 				</select>
-
 				<select 
 				name='order'	
 				value={props.order}
@@ -108,8 +93,6 @@ export default class Filter extends React.Component {
 				
 				</select>
 			</span>
-
-
 				<filter>
 					<List>
 						{this.state.jobs
@@ -117,22 +100,16 @@ export default class Filter extends React.Component {
 								return job.title.toLowerCase().includes(this.state.query.toLowerCase()) ||
 								job.description.toLowerCase().includes(this.state.query.toLowerCase())
 							})
-
 							.filter(job => {
 								return this.state.minPrice === "" || job.price >= this.state.minPrice 
 							})
-
-
 							.filter(job => {
 								return this.state.maxPrice === "" || job.price <= this.state.maxPrice 
 							})
-
-
 						.sort((currentJob,nextJob) =>{
 							switch (this.state.sortingParameter){
 								case "title":
 									return this.state.order * currentJob.title.localeCompare(nextJob.title)
-
 								case "dueDate":
 									return this.state.order * (new Date(currentJob.dueDate).getTime() - new Date(nextJob.dueDate).getTime())
 								default:
@@ -142,7 +119,6 @@ export default class Filter extends React.Component {
 						.map(job =>{
 							return <Card key={job.id} job={job} />
 					})}
-
 				</List>
 	
 				</filter>
