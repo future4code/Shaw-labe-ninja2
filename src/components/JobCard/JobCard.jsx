3e0abb69451b4ff27974630f47636cd19789e04e
LayaneB <layanejuvito@gmail.com>
2022-04-15 15:02:41 -0300

    Adicionando o input da imagem e adaptando o request

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -74,14 +74,14 @@ export default class JobCard extends React.Component {
 					style={{ width: '22vw', height: '22vw'}} /// these are optional style, it is not necessary
 
 				>
-					<FrontSide style={{  backgroundImage: 'radial-gradient(circle at 27.95% 27.95%, #4c4732 0, #000104 100%, #000000 50%)', padding:'5px' }} animationDuration={1000}>
+					<FrontSide style={{  backgroundImage: 'radial-gradient(circle at 27.95% 27.95%, #4c4732 0, #000104 100%, #000000 50%)', padding:'8px' ,height:'100%'}} animationDuration={1000}>
 						<h3 style={{ fontSize:'1.5em', textAlign:'center', marginBottom:'10px',color:'#FFE600'}}>{this.props.title}</h3>
 						<ImgContainer>
-							<Img style={{border:'1px solid #FFE600'}} src={'https://picsum.photos/250/150'} alt={''} />
+							<Img style={{border:'1px solid #FFE600'}} src={this.props.image} alt={''} />
 							<p style={{color:'white', fontSize:'1.2em'}}>R$ {this.props.price},00</p>
 						</ImgContainer>
 					</FrontSide>
-					<BackSide style={{ backgroundImage: 'radial-gradient(circle at 27.95% 27.95%, #4c4732 0, #000104 50%, #000000 100%)', boxShadow: '2px 2px 10px black', color: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '2px',padding:'5px' }} animationDuration={1000}>
+					<BackSide style={{ backgroundImage: 'radial-gradient(circle at 27.95% 27.95%, #4c4732 0, #000104 50%, #000000 100%)', boxShadow: '2px 2px 10px black', color: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '2px',padding:'8px' }} animationDuration={1000}>
 						<h3 style={{ color:' #FFE600', fontSize:'24px'}}>{this.props.title}</h3>
 						<p style={{ textAlign: 'center'}}>{this.props.description}</p>
 						<Rating name="read-only" value={this.state.rating} readOnly />
